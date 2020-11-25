265801be05fb31f2d24995a85b73b4fcbf6db52d
stefanionescu <stefanionescu@protonmail.com>
2020-11-25 16:53:56 +0000

    Update deps

--- src/test/DebtAuctionInitialParameterSetter.t.sol
+++ src/test/DebtAuctionInitialParameterSetter.t.sol
@@ -69,7 +69,7 @@ contract DebtAuctionInitialParameterSetterTest is DSTest {
         hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
         hevm.warp(604411200);
 
-        systemCoin = new DSToken("RAI");
+        systemCoin = new DSToken("RAI", "RAI");
         treasury = new MockTreasury(address(systemCoin));
         accountingEngine = new AccountingEngine();
 
